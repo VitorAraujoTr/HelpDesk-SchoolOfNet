068c4af0351720e3ee400c9cd84cf92447ed04b9
Vitor Araújo <Vitor Arajo@W8-PCN>
2018-04-23 17:46:34 -0300

    Início de projeto

---
+++ HelpDesk-SchoolOfNet/src/main/java/com/schoolofnet/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.schoolofnet;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelpDeskSchoolOfNetApplication.class);
+	}
+
+}
